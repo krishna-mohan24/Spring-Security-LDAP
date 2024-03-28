d3240a4271bf37a12ae8cb6abb195e67d7b48775
krishna-mohan24 <116231623+krishna-mohan24@users.noreply.github.com>
2024-03-28 01:25:25 -0400

    first commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+
+    }
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http    .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                ;
+    }
+}
